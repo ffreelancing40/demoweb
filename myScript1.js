b4d8f84ac9ba58c5c0649824d392132bac153a4e
Rit <ffreelancing40@gmail.com>
2025-07-29 20:28:00 +0530

    Update myScript1.js
    function edit1

--- myScript1.js
+++ myScript1.js
@@ -1,1 +1,11 @@
-alert("Your Browser is Hacked");
+function showAlert() {
+    alert("Button clicked!");
+}
+
+const button = document.getElementById("myButton"); //
+
+if (button) {
+    button.addEventListener("click", showAlert); //
+} else {
+    console.error("Button with ID 'myButton' not found.");
+}
